970604e559d6046db6fff85d9e1a78426ede4e40
Lolevtexa <ceba.kek@gmail.com>
2025-02-14 16:05:09 +0300

    format code for consistency and readability across multiple files

--- k1/s2/proga/itog/src/main.cpp
+++ k1/s2/proga/itog/src/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include "elements/passive/TextLabel.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+  TextLabel label(3, 15, "Hello, World!");
 
-    return 0;
+  for (int i = 0; i < label.displayData.size(); i++) {
+    for (int j = 0; j < label.displayData[i].size(); j++) {
+      std::cout << label.displayData[i][j];
+    }
+    std::cout << std::endl;
+  }
+
+  return 0;
 }
